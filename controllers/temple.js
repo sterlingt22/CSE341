849cd76fc9012e0a6f47811f33fe4fed9e37bb90
sterlingt22 <sktucker21@byui.edu>
2024-01-31 00:51:40 -0700

    Added API if needed comment

--- controllers/temple.js
+++ controllers/temple.js
@@ -5,6 +5,9 @@ const apiKey =
   'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N';
 
 exports.create = (req, res) => {
+  /* 
+  #swagger.description = 'API if needed: 'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N'
+  */
   // Validate request
   if (!req.body.name) {
     res.status(400).send({ message: 'Content can not be empty!' });
