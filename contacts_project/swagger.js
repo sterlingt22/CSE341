f4c3295616b9a7bf7a833e5f2a8ed0c2011deff2
sterlingt22 <sktucker21@byui.edu>
2024-02-01 15:21:54 -0700

    fixed issue in swagger.js

--- contacts_project/swagger.js
+++ contacts_project/swagger.js
@@ -10,7 +10,7 @@ const doc = {
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFiles = ['./contacts_project/routes/index.js'];
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
