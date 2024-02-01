83b9482fbc3496e8e33e0e767dd4f34eb64ea90c
sterlingt22 <sktucker21@byui.edu>
2024-02-01 14:23:45 -0700

    More changes wk4 personal

--- contacts_project/routes/index.js
+++ contacts_project/routes/index.js
@@ -1,32 +1,7 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const router = express.Router();
 
-let _db;
+router.use('/', require('./swagger'));
+router.use('/contacts', require('./contacts'));
 
-const initDb = (callback) => {
-  if (_db) {
-    console.log('Db is already initialized!');
-    return callback(null, _db);
-  }
-  MongoClient.connect(process.env.MONGODB_URI)
-    .then((client) => {
-      _db = client;
-      callback(null, _db);
-    })
-    .catch((err) => {
-      callback(err);
-    });
-};
-
-const getDb = () => {
-  if (!_db) {
-    throw Error('Db not initialized');
-  }
-  return _db;
-};
-
-module.exports = {
-  initDb,
-  getDb
-};
+module.exports = router;
