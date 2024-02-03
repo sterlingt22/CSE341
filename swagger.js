9425fb75e8d4af9eb5817ad1e3d88c8ec70bc33e
sterlingt22 <sktucker21@byui.edu>
2024-02-03 16:50:56 -0700

    More changes due to errors

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Temple API',
+    description: 'contacts API',
   },
   host: 'localhost:3000',
   schemes: ['http'],
